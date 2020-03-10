718aa2bc5e3e621420b7c013b0a21b00c57a000d
CodierungVonMechatronic <48565570+CodierungVonMechatronic@users.noreply.github.com>
2020-03-10 19:22:09 +0300

    Add files via upload

---
+++ incrementOperators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	
+	
+	int i = 0;
+	int j = i * ++i;
+	int k = j++ + ++i;
+	i = j + k;
+	k = i + j;
+	
+	printf("\n%d\n",i);
+	printf("%d\n",j);
+	printf("%d\n",k);
+
+	
+	
+
+	return 0;
+}
